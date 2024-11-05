548bd2ec40ec548daeb1ecbb2fcb27edb75db1b8
vishalj2805 <vishalj@Vishals-Mac-mini.local>
2024-11-05 12:03:31 +0530

    Made Changes in TestNGRunner File

--- src/test/java/base/TestNGRunner.java
+++ src/test/java/base/TestNGRunner.java
@@ -3,6 +3,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/test/java/features/SignIn.feature", glue = "stepDefs", monochrome = true)
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefs", monochrome = true)
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 }
